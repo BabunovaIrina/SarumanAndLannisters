7682c1d74820e05fb0f840603d32e41daf56124c
irinababunova <xleyla@gmail.com>
2023-10-28 23:22:15 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        final int numberSoldiersLannister = 2470;
+        System.out.println("Количество пехотинцев у Ланнистеров - " + numberSoldiersLannister);
+
+        final int numberOrksSaruman = 5349;
+        System.out.println("Количество орков в войске Сарумана - " + numberOrksSaruman);
+
+        final int totalSoldiersLannisterAndSaruman = numberSoldiersLannister + numberOrksSaruman;
+        System.out.println("Общее количество пехоты в союзе Ланнистеров и Сарумана = " + totalSoldiersLannisterAndSaruman);
+    }
+}
